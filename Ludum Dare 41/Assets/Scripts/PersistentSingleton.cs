225e92209f771c57ff7f6e4beb7a83629253bea6
wholesalebirbs <36047288+wholesalebirbs@users.noreply.github.com>
2018-04-20 22:31:23 -0700

    Initial Set Up
    Set up Towers, Player, Turrets, and Pickups

---
+++ Ludum Dare 41/Assets/Scripts/PersistentSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentSingleton<T> : MonoBehaviour where T : Component
+{
+    //protected static T _instance;
+    //protected bool _enabled;
+
+
+    //public static T Instance
+    //{
+    //    get
+    //    {
+    //        if (_instance == null)
+    //        {
+    //            _instance = FindObjectOfType<T>();
+    //            if (_instance == null)
+    //            {
+    //                GameObject obj
+    //            }
+    //        }
+    //    }
+    //}
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
